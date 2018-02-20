a5ff06911b8589f42778abc4dd2b4682ec7790b1
Yuri Istomin <yura.ist@icloud.com>
2018-02-21 00:01:54 +0300

    Added user authentication functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 # Initialize instances of libraries
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -17,10 +21,15 @@ def create_app(config_name):
     # Initialize the app in instances of libraries
     db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
 
     # Register a main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Register an auth blueprint
+    from .auth import auth as auth_bluepring
+    app.register_blueprint(auth_bluepring, url_prefix='/auth')
 
     # Register a bot blueprint
     from .bot import bot as bot_blueprint
---
+++ app/auth/views.py
@@ -0,0 +1,38 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user
+from . import auth
+from .forms import LoginForm, RegisterForm
+from .. import db
+from ..models import User
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        flash('You have been registered.')
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form=form)
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
